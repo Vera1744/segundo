3c16359de76ac8cb6781850f0dded7301dda5de2
Vera Blok <xitraia@mail.ru>
2024-12-28 00:24:47 +0300

    task miles

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        int amount = 135;
+        int x = 20;
+        int miles = amount / x;
+
+        System.out.println("Вам начислено " + miles + " миль, спасибо, что летаете с нами.");
+
+    }
+}
